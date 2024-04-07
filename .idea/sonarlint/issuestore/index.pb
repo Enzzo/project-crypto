587b272ad804c91504cbbd4124a67a9eeaa86759
Sergey <enzzomatrix@gmail.com>
2024-04-07 21:36:47 +1100

    upd

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,3 +6,5 @@
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 t
 Dsrc/main/java/ru/javarush/vasilev/cryptoanalizer/CryptoAnalizer.java,a\9\a92f4f968179e277dd9d72b9788764e2fd098f46
+l
+<src/main/java/ru/javarush/vasilev/cryptoanalizer/Cypher.java,1\5\15da6afadd2f5f37fd5cbbf0018c455a1fa14a57
--- src/main/java/ru/javarush/vasilev/cryptoanalizer/Cypher.java
+++ src/main/java/ru/javarush/vasilev/cryptoanalizer/Cypher.java
@@ -1,40 +1,113 @@
 package ru.javarush.vasilev.cryptoanalizer;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Cypher {
+    // Алфавит не содержит буквы ё, й, ю и некоторые символы из задания:
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
-    public static void encrypt(File source, File destination, int key)throws IOException{
-        try(FileReader reader = new FileReader(source);
-            FileWriter os = new FileWriter(destination)){
+    static{
+        // Сортируем алфавит для возможности бинарного поиска
+        Arrays.sort(ALPHABET);
+    }
 
+    //  +---------------------------------------------------------------+
+    //  |   Шифрует содержимое source и сохраняет в destination         |
+    //  +---------------------------------------------------------------+
+    //  |   srcFile - файл, содержащий исходный текст                   |
+    //  |   dstFile - файл, в который записывается зашифрованный        |
+    //  |   результат                                                   |
+    //  |   key - величина, на которую сдвигается исходный текст        |
+    //  +---------------------------------------------------------------+
+    public static void encrypt(File srcFile, File dstFile, int key){
+        try(FileReader reader = new FileReader(srcFile);
+            FileWriter os = new FileWriter(dstFile)){
             char[] buffer = new char[1024];
             int len = 0;
             while((len = reader.read(buffer)) > 0){
-                // TODO: сдвигаем, а затем записывавем в файл-назначение:
-
                 for(int i = 0; i < len; ++i){
-                    for(int j = 0; j < ALPHABET.length; ++j){
-                        char ch = buffer[i];
-                        char alp = ALPHABET[j];
-                        if(ch == alp){
-                            int off = (j + key) % (ALPHABET.length);
-                            buffer[i] = ALPHABET[off];
-                            break;
-                        }
+                    int idx = Arrays.binarySearch(ALPHABET, buffer[i]);
+                    if(0 <= idx) {
+                        int offs = roll(idx, key, ALPHABET.length);
+                        buffer[i] = ALPHABET[offs];
                     }
                 }
-
                 os.write(buffer, 0, len);
             }
         }
+        catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
-    public static void decrypt(File source, File destination, int key){}
-    public static void brutForce(File source, File destination, int key){}
-    public static void statisticalDecrypt(File source, File destination, int key){}
+
+    //  +---------------------------------------------------------------+
+    //  |   Расшифровывает содержимое source и сохраняет в destination  |
+    //  +---------------------------------------------------------------+
+    //  |   srcFile - файл, содержащий исходный текст                   |
+    //  |   dstFile - файл, в который записывается расшифрованный       |
+    //  |   результат                                                   |
+    //  |   key - величина, на которую сдвигается исходный текст        |
+    //  +---------------------------------------------------------------+
+    public static void decrypt(File srcFile, File dstFile, int key){
+        encrypt(srcFile, dstFile, -key);
+    }
+
+    //  +---------------------------------------------------------------+
+    //  |   Перебирает все возможные расшифрованные варианты исходного  |
+    //  |   файла                                                       |
+    //  +---------------------------------------------------------------+
+    //  |   srcFile - файл, содержащий исходный текст                   |
+    //  |   dstDir  - папка, в которую будут сохранены результаты брута |
+    //  +---------------------------------------------------------------+
+    //  |   Если директории назначения не существует, то метод создаёт  |
+    //  |   эту директорию.                                             |
+    //  |   Если директорию создать невозможно, то метод завершается    |
+    //  |   без результата                                              |
+    //  |   В директории назначения создаются файлы-результаты брута    |
+    //  +---------------------------------------------------------------+
+    public static void brutForce(File srcFile, File dstDir){
+        try {
+            if (!Files.exists(Path.of(dstDir.getPath()))) {
+                Files.createDirectory(Path.of(dstDir.getPath()));
+            }
+        }
+        catch(IOException e){
+            System.out.printf("Can't create path<%s>\n", dstDir);
+            return;
+        }
+        for(int i = 1; i < ALPHABET.length; ++i){
+            String filePath = String.format("%s\\%2s.txt", dstDir.getPath(), i).replaceAll(" ", "0");
+            File dstFile = new File(filePath);
+            encrypt(srcFile, dstFile, i);
+        }
+    }
+
+    public static void statisticalDecrypt(File sourceFile, File destination, int key){}
+
+    //  +---------------------------------------------------------------+
+    //  |   Смещает курсор на step символов                             |
+    //  +---------------------------------------------------------------+
+    //  |   pos - изначальная позиция курсора                           |
+    //  |   step - шаг, на который должен быть смещён курсор            |
+    //  |   size - общий размер (он же ограничитель) коллекции          |
+    //  +---------------------------------------------------------------+
+    //  |   return новую позицию курсора с учётом смещения (step)       |
+    //  +---------------------------------------------------------------+
+    private static int roll(int pos, int step, int size){
+        //  если шаг отрицательный, то крутим курсор в обратном направлении
+        if(step < 0){
+            step = size + step % size;
+        }
+        int result = (pos + step) % size;
+        return result;
+    }
 }
